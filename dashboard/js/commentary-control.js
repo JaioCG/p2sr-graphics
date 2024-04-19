64c50c12e852562abafd7f787a4ac9474dcd6b0a
Jaio <jaio0701@gmail.com>
2024-04-18 21:01:14 -0400

    refactor: arrays for commentary replicant

--- dashboard/js/commentary-control.js
+++ dashboard/js/commentary-control.js
@@ -1,12 +1,12 @@
 // Create replicants
-const comm1Rep = nodecg.Replicant('comm1', {defaultValue: 'Comm 1'});
-const comm2Rep = nodecg.Replicant('comm2', {defaultValue: 'Comm 2'});
-const comm3Rep = nodecg.Replicant('comm3', {defaultValue: 'Comm 3'});
+const commNamesRep = nodecg.Replicant('commNames', {defaultValue: ['Comm 1', 'Comm 2', 'Comm 3']});
 
 // Update replicants
 document.getElementById('submit').addEventListener('click', function() {
     console.log('Updating commentary');
-    comm1Rep.value = document.getElementById('comm1').value;
-    comm2Rep.value = document.getElementById('comm2').value;
-    comm3Rep.value = document.getElementById('comm3').value;
+    commNamesRep.value = [
+        document.getElementById('comm1').value,
+        document.getElementById('comm2').value,
+        document.getElementById('comm3').value
+    ];
 });
