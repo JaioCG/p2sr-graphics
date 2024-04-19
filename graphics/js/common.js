c3551827550d6d8f98ecdf521147c724dd64f2ca
Jaio <jaio0701@gmail.com>
2024-04-19 16:43:17 -0400

    refactor: delete runner-control
    Ended up using speedcontrol lol

--- graphics/js/common.js
+++ graphics/js/common.js
@@ -1,6 +1,5 @@
 // Create replicants
 const commNamesRep = nodecg.Replicant('commNames', {defaultValue: ['Comm 1', 'Comm 2', 'Comm 3']});
-const runnerNamesRep = nodecg.Replicant('runnerNames', {defaultValue: ['Runner 1', 'Runner 2', 'Runner 3', 'Runner 4']});
 
 // Update replicants
 commNamesRep.on('change', function(newValue) {
@@ -8,12 +7,4 @@ commNamesRep.on('change', function(newValue) {
     document.getElementById('comm1').innerHTML = newValue[0];
     document.getElementById('comm2').innerHTML = newValue[1];
     document.getElementById('comm3').innerHTML = newValue[2];
-});
-
-runnerNamesRep.on('change', function(newValue) {
-    console.log('Updating runners');
-    document.getElementById('runner1').innerHTML = newValue[0];
-    document.getElementById('runner2').innerHTML = newValue[1];
-    document.getElementById('runner3').innerHTML = newValue[2];
-    document.getElementById('runner4').innerHTML = newValue[3];
 });
