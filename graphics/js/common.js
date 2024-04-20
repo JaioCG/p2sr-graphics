d0f6d704bdd451bb637348cd8f7080bf0aa6818a
Jaio <jaio0701@gmail.com>
2024-04-20 16:21:29 -0400

    fix: oh god last minute changes

--- graphics/js/common.js
+++ graphics/js/common.js
@@ -2,6 +2,7 @@
 $(() => {
     // Create replicants
     const commNamesRep = nodecg.Replicant('commNames', {defaultValue: ['Comm 1', 'Comm 2', 'Comm 3']});
+    const playerNamesRep = nodecg.Replicant('playerNames', {defaultValue: ['Player 1', 'Player 2', 'Player 3', 'Player 4']});
 
     // Update replicants
     commNamesRep.on('change', function(newValue) {
@@ -9,6 +10,13 @@ $(() => {
         document.getElementById('comm1').innerHTML = newValue[0];
         document.getElementById('comm2').innerHTML = newValue[1];
         document.getElementById('comm3').innerHTML = newValue[2];
+    });
+    playerNamesRep.on('change', function(newValue) {
+        console.log('Updating players');
+        document.getElementById('player1').innerHTML = newValue[0];
+        document.getElementById('player2').innerHTML = newValue[1];
+        document.getElementById('player3').innerHTML = newValue[2];
+        document.getElementById('player4').innerHTML = newValue[3];
     });
 
 	// The bundle name where all the run information is pulled from.
@@ -43,10 +51,10 @@ $(() => {
 			// If a team has multiple players, this currently just outputs them in a comma'd list.
 			if (team) {
 				players.html(team.players.map((player) => player.name).join(', '));
-                player1.html(team.players[0].name);
-                player2.html(team.players[1].name);
-                player3.html(team.players[2].name);
-                player4.html(team.players[3].name);
+                // player1.html(team.players[0].name);
+                // player2.html(team.players[1].name);
+                // player3.html(team.players[2].name);
+                // player4.html(team.players[3].name);
 			}
 		}
 	}
