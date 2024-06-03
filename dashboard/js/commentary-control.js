e87a286b3f2cb93c131bec94bbfeeb43b94c7936
Jaio <jaio0701@gmail.com>
2024-06-03 13:56:44 -0400

    just de-jank/remove some bullshit

--- dashboard/js/commentary-control.js
+++ dashboard/js/commentary-control.js
@@ -1,6 +1,5 @@
 // Create replicants
 const commNamesRep = nodecg.Replicant('commNames', {defaultValue: ['Comm 1', 'Comm 2', 'Comm 3']});
-const playerNamesRep = nodecg.Replicant('playerNames', {defaultValue: ['Player 1', 'Player 2', 'Player 3', 'Player 4']});
 
 // Update replicants
 document.getElementById('submit').addEventListener('click', function() {
@@ -11,13 +10,3 @@ document.getElementById('submit').addEventListener('click', function() {
         document.getElementById('comm3').value
     ];
 });
-
-function updatePlayers() {
-    console.log('Updating players');
-    playerNamesRep.value = [
-        document.getElementById('player1').value,
-        document.getElementById('player2').value,
-        document.getElementById('player3').value,
-        document.getElementById('player4').value
-    ];
-}
