646c4d262ffc913bd2ca3edfba361fe854c692a6
Jaio <jaio0701@gmail.com>
2024-06-05 10:17:43 -0400

    therun.gg output formatting

--- graphics/js/therun.js
+++ graphics/js/therun.js
@@ -19,22 +19,38 @@ nodecg.listenFor('therunP2', (newVal) => {
 // Function to update all data for a player and add to array
 function updateSplitData(data)
 {
-    // Get speedrun.com PB rank
-    const srcClient = new SpeedrunClient();
-    srcClient.runs
+    // Random Math Stuff
+    var  possibleTimesave = data.run.splits[data.run.currentSplitIndex].pbSplitTime - data.run.splits[data.run.currentSplitIndex].bestPossible;
 
     // Personal Best Time (trgg)
-    // Rank on Leaderboards (src) (have to learn other api ughhh)
     // Sum of Best (trgg)
     // Best Possible Time (trgg)
     // Current Split (trgg)
-    // Possible Time Save (trgg) (dont know how to fucking get)
+    // Possible Time Save (trgg)
     return {
-        'pbTime'            : data.run.pb,
-        'srcRank'           : '',
-        'sumOfBest'         : data.run.sob,
-        'bestPossibleTime'  : data.run.bestPossible,
+        'pbTime'            : msToTime(data.run.pb),
+        'sumOfBest'         : msToTime(data.run.sob),
+        'bestPossibleTime'  : msToTime(data.run.bestPossible),
         'currentSplit'      : data.run.currentSplitName,
-        'possibleTimeSave'  : '',
+        'possibleTimeSave'  : msToTime(possibleTimesave)
     };
+}
+
+// Util function to convert ms to more readable time format
+function msToTime(duration)
+{
+    var milliseconds = Math.floor((duration % 1000) / 100),
+        seconds = Math.floor((duration / 1000) % 60),
+        minutes = Math.floor((duration / (1000 * 60)) % 60),
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+  
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+    if (hours == "00") {
+      return minutes + ":" + seconds + "." + milliseconds;
+    } else {
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    }
 }
